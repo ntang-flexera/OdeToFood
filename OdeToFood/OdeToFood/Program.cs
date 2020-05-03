064e853a24252627787b39e6e405a8fcc4b4cf7d
ntang-flexera <nxtang17@gmail.com>
2020-05-03 07:23:21 -0500

    started the project from scratch. -added OdeToFood project -added OdeToFood solution file

--- OdeToFood/OdeToFood/Program.cs
+++ OdeToFood/OdeToFood/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -17,24 +13,14 @@ namespace OdeToFood
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            MigrateDatabase(host);
-                
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private static void MigrateDatabase(IWebHost host)
-        {
-            using(var scope = host.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
-                db.Database.Migrate();
-            }
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -1,11 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -21,26 +23,7 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddDbContextPool<OdeToFoodDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
-            });
-
-            services.AddScoped<IRestaurantData, SqlRestaurantData>();
-            //services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
-
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            // aspnetcore30
             services.AddRazorPages();
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,38 +36,21 @@ namespace OdeToFood
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
-            app.Use(SayHelloMiddleware);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseNodeModules();
 
-            // aspnetcore30
-            app.UseRouting();            
-            app.UseEndpoints(e =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                e.MapRazorPages();
-                e.MapControllers();
+                endpoints.MapRazorPages();
             });
-        }
-
-        private RequestDelegate SayHelloMiddleware(
-                                    RequestDelegate next)
-        {
-            return async ctx =>
-            {
-
-                if (ctx.Request.Path.StartsWithSegments("/hello"))
-                {
-                    await ctx.Response.WriteAsync("Hello, World!");
-                }
-                else
-                {
-                    await next(ctx);                    
-                }
-            };
         }
     }
 }
