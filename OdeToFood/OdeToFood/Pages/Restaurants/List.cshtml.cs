1b7e617af4b9907b082b20ce3ae40e0242aa7df8
ntang-flexera <nxtang17@gmail.com>
2020-05-03 22:55:12 -0500

    added Search function to the List page

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        public string Message{get;set;}
+        public string Message { get; set; }
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        [BindProperty(SupportsGet=true)]
+        public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration config)
+        public List<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantByName(SearchTerm).ToList();
         }
     }
 }
